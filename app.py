5907b67ad693e8fb2b74330a4768e71abd4c3a5d
saurabh <saurabh10032001.com>
2026-02-05 21:33:21 +0530

    4

--- app.py
+++ app.py
@@ -5,6 +5,8 @@ from datetime import datetime
 import pytz
 from dhanhq import dhanhq
 
+time.sleep(5)
+
 ACCESS_TOKEN = os.getenv('ACCESS_TOKEN')
 BOT_TOKEN = os.getenv('BOT_TOKEN')
 CHAT_ID = os.getenv('CHAT_ID')
